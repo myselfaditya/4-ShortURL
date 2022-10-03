466f8f0ccbdfaeadf180ae54569f63a42fa4f88e
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2022-10-03 16:36:01 +0530

    redis

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -2,6 +2,34 @@ const urlModel = require("../model/urlModel");
 const validUrl = require("valid-url");
 const shortid = require("shortid");
 
+const redis = require("redis");
+
+const { promisify } = require("util");
+
+//Connect to redis
+const redisClient = redis.createClient(
+  15432,
+  "redis-15432.c212.ap-south-1-1.ec2.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+redisClient.auth("Jsq2dv5p4Vn1tUKz8DlwXWEK6sgWd5XE", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");
+});
+
+
+
+//1. connect to the server
+//2. use the commands :
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
 const createUrl = async function (req, res) {
   try {
     const longUrl = req.body.longUrl;
@@ -43,11 +71,20 @@ const getUrl = async function (req, res) {
 
     if (!shortid.isValid(urlCode)) return res.status(400).send({ status: false, message: 'Please provide valid UrlCode' })
 
+    let cacheUrl = await GET_ASYNC(`${urlCode}`)// we are getting in string format
+    let objCache= JSON.parse(cacheUrl)// converting into object format  
+
+    
+    if (objCache) {
+      return res.status(302).redirect(objCache.longUrl)
+    }
+
     const checkUrlCode = await urlModel.findOne({ urlCode: urlCode })
 
     if (!checkUrlCode) return res.status(404).send({ status: false, message: 'UrlCode not found' })
-
+    await SET_ASYNC(`${urlCode}`, JSON.stringify(checkUrlCode))
     return res.status(302).redirect(checkUrlCode.longUrl)
+
   }
   catch (err) {
     res.status(500).send({ status: false, message: err.message })
